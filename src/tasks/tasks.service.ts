616867e2cb28d99aec5544abcde64b4ac41046f9
Inon Bercu <inonbercu@Inons-MacBook-Pro.local>
2021-09-15 17:45:02 +0300

    create task and get all tasks are routes are done

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuidV4 } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const newTask: Task = {
+      id: uuidV4(),
+      title: title,
+      description: description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(newTask);
+
+    return newTask;
+  }
+}
